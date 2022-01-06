2647d2f8c4963c394a2eb0468f734eab24c022f7
Matheus Ferreira <matheus.44300@gmail.com>
2022-01-06 01:22:50 -0300

    Updated Search Design

--- lib/app/screens/groups/components/search_widget.dart
+++ lib/app/screens/groups/components/search_widget.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class SearchWidget extends StatelessWidget {
-  const SearchWidget({Key? key}) : super(key: key);
+  final Function(String)? onChanged;
+  const SearchWidget({
+    Key? key,
+    this.onChanged,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: Colors.white,
+      style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
         hintText: 'Pesquisar',
+        hintStyle: const TextStyle(
+          color: Color.fromARGB(170, 255, 255, 255),
+        ),
         contentPadding: const EdgeInsets.only(left: 16.0),
         fillColor: const Color.fromARGB(50, 255, 255, 255),
         filled: true,
@@ -15,25 +24,20 @@ class SearchWidget extends StatelessWidget {
           Icons.search,
           color: Colors.white,
         ),
-        border: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: Colors.transparent,
-          ),
-          borderRadius: BorderRadius.circular(24),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: Colors.transparent,
-          ),
-          borderRadius: BorderRadius.circular(24),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: Colors.transparent,
-          ),
-          borderRadius: BorderRadius.circular(24),
-        ),
+        border: _defaultBorder(),
+        enabledBorder: _defaultBorder(),
+        focusedBorder: _defaultBorder(),
       ),
+      onChanged: onChanged,
+    );
+  }
+
+  _defaultBorder() {
+    return OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent,
+      ),
+      borderRadius: BorderRadius.circular(24),
     );
   }
 }
