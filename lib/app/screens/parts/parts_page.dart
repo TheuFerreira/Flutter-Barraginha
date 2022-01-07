84fc2401e76e8f6f116809923a39329b217271a9
Matheus Ferreira <matheus.44300@gmail.com>
2022-01-07 20:48:15 -0300

    Componentized Item Part

--- lib/app/screens/parts/parts_page.dart
+++ lib/app/screens/parts/parts_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_barraginha/app/shared/components/text_field_widget.dart';
 
 import 'components/item_info_widget.dart';
+import 'components/item_part_widget.dart';
 
 class PartsPage extends StatefulWidget {
   const PartsPage({Key? key}) : super(key: key);
@@ -127,6 +128,7 @@ class _PartsPageState extends State<PartsPage> {
                       itemCount: 3,
                       itemBuilder: (builder, i) {
                         return const ItemPartWidget();
+                        // TODO: Edit Part
                       },
                     ),
                   ],
@@ -135,158 +137,6 @@ class _PartsPageState extends State<PartsPage> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ItemPartWidget extends StatelessWidget {
-  const ItemPartWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      margin: const EdgeInsets.symmetric(
-        vertical: 8.0,
-        horizontal: 8.0,
-      ),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(24),
-          topLeft: Radius.circular(24),
-          topRight: Radius.circular(24),
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Stack(
-          alignment: AlignmentDirectional.topCenter,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12.0,
-                horizontal: 16.0,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Trecho 1',
-                        style: TextStyle(
-                          color: Color(0xFF666666),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 26.0,
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.info),
-                        iconSize: 32,
-                        color: const Color(0xFF666666),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Tamanho',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Color(0xFF666666),
-                            ),
-                          ),
-                          Text(
-                            'Raio: 2m',
-                            style: TextStyle(
-                              height: 1.5,
-                              fontSize: 16.0,
-                              color: Color(0xFFA8A8A8),
-                            ),
-                          ),
-                          Text(
-                            'Profundidade: 2m',
-                            style: TextStyle(
-                              height: 1.5,
-                              fontSize: 16.0,
-                              color: Color(0xFFA8A8A8),
-                            ),
-                          ),
-                          Text(
-                            'Volume: 20000L',
-                            style: TextStyle(
-                              height: 1.5,
-                              fontSize: 16.0,
-                              color: Color(0xFFA8A8A8),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            'Bolsões',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Color(0xFF666666),
-                            ),
-                          ),
-                          Text(
-                            '6',
-                            style: TextStyle(
-                              height: 1.5,
-                              fontSize: 16.0,
-                              color: Color(0xFFA8A8A8),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: const [
-                          Text(
-                            'Distância',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Color(0xFF666666),
-                            ),
-                          ),
-                          Text(
-                            '200m',
-                            style: TextStyle(
-                              height: 1.5,
-                              fontSize: 16.0,
-                              color: Color(0xFFA8A8A8),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: IconButton(
-                icon: Icon(Icons.arrow_forward),
-                iconSize: 32,
-                color: Color(0xFF00695C),
-                onPressed: () {},
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
