eb3de1b115467f6edb4eecad064ecab584c953ad
Matheus Ferreira <matheus.44300@gmail.com>
2022-02-16 19:26:03 -0300

    Updated Design of Tutorial Page

--- lib/app/screens/tutorial/components/main_container.dart
+++ lib/app/screens/tutorial/components/main_container.dart
@@ -1,11 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter_barraginha/app/app.dart';
 import 'package:flutter_barraginha/app/screens/tutorial/components/curved_container.dart';
 
 class MainContainer extends StatefulWidget {
-  const MainContainer({ Key? key }) : super(key: key);
+  const MainContainer({Key? key}) : super(key: key);
 
   @override
   _TutorialPageState createState() => _TutorialPageState();
@@ -14,61 +11,44 @@ class MainContainer extends StatefulWidget {
 class _TutorialPageState extends State<MainContainer> {
   @override
   Widget build(BuildContext context) {
-    return ClipPath
-      (
-        clipper: CurvedContainer(),
-        child: Container
-        (
-          padding: EdgeInsets.only(bottom: 0),
-          child: Stack
-          (
-            children: 
-            [
-              Expanded
-              (
-                child: Container
-                (
+    return ClipPath(
+      clipper: CurvedContainer(),
+      child: Container(
+        padding: EdgeInsets.only(bottom: 0),
+        child: Stack(
+          children: [
+            Expanded(
+              child: Container(
                 width: double.infinity,
-                height: 600,//MediaQuery.of(context).size.height/2,
-                decoration: BoxDecoration
-                (
-                 
-                  color: Color(0xffF1F1F1)
+                height: 600, //MediaQuery.of(context).size.height/2,
+                decoration: const BoxDecoration(
+                  color: Color(0xffF1F1F1),
                 ),
-                  child: Column
-                  (
-                    children: [
-                      ClipPath
-                      (
-                        clipper: CurvedContainer(),
-                
-                        child: Container
-                        (
-                          color:Colors.white,
-                          width: double.infinity,
-                          height: 85,
-                        ),
+                child: Column(
+                  children: [
+                    ClipPath(
+                      clipper: CurvedContainer(),
+                      child: Container(
+                        color: Colors.white,
+                        width: double.infinity,
+                        height: 85,
                       ),
-
-                      ClipRRect
-                      (
-                        borderRadius: BorderRadius.circular(30),
-                        child: Image.asset
-                        (
-                          'images/if.png',
-                          width: 100,//MediaQuery.of(context).size.width-50,
-                          height: 100,//MediaQuery.of(context).size.width-50,
-                        ),
-                      )
-                    ],
-                  )
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset(
+                        'images/if.png',
+                        width: 100, //MediaQuery.of(context).size.width-50,
+                        height: 100, //MediaQuery.of(context).size.width-50,
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ],  
-          ),
+            ),
+          ],
         ),
-      );
-    
+      ),
+    );
   }
 }
-
--- lib/app/screens/tutorial/tutorial_page.dart
+++ lib/app/screens/tutorial/tutorial_page.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_barraginha/app/screens/tutorial/components/bottom_container.dart';
 import 'package:flutter_barraginha/app/screens/tutorial/components/curved_container.dart';
-import 'package:flutter_barraginha/app/screens/tutorial/components/main_container.dart';
 
 class TutorialPage extends StatefulWidget {
-  const TutorialPage({ Key? key }) : super(key: key);
+  const TutorialPage({Key? key}) : super(key: key);
 
   @override
   _TutorialPageState createState() => _TutorialPageState();
@@ -15,59 +12,88 @@ class TutorialPage extends StatefulWidget {
 class _TutorialPageState extends State<TutorialPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-      body: Column
-      (
-        children: 
-        [
-          Expanded
-          (
-            flex: 1,
-            child: Container
-            (
-              color: Colors.amber,
-            )
-          ),
-
-          Expanded
-          (
-            flex: 3,
-            child: MainContainer()
-          ),
-
-          Expanded
-          (
-            
-            child: Stack
-            (
-              fit: StackFit.loose,
-              children: 
-              [
-                Container(color: Colors.blue,),
-
-                Positioned
-                (
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  bottom: 0,
-                  child: RotatedBox
-                (
-                  quarterTurns: 2,
-                  child: ClipPath
-                  (
-                    clipper: BottomContainer(),
-                    child: Container
-                    (
-                      color: Colors.black,
-                    ),
-                  ),
+    return Scaffold(
+      backgroundColor: const Color(0xffF1F1F1),
+      body: Column(
+        children: [
+          Expanded(
+            child: ClipPath(
+              clipper: CurvedContainer(),
+              child: Container(
+                width: double.infinity,
+                color: Colors.white,
+                child: Center(
+                  child: Text('Título'),
                 ),
-                )
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Text('ola'),
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        RotatedBox(
+                          quarterTurns: 2,
+                          child: ClipPath(
+                            clipper: BottomContainer(),
+                            child: Container(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 35,
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Stack(
+                            children: [
+                              RotatedBox(
+                                quarterTurns: 2,
+                                child: ClipPath(
+                                  clipper: BottomContainer(),
+                                  child: Container(
+                                    width: double.infinity,
+                                    color: Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Descição'),
+                            Text('Bolinhas'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
